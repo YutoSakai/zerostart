950a3cd3b1748d00dc74187a0df033bde050be0c
YutoSakai <43488753+YutoSakai@users.noreply.github.com>
2019-04-26 18:34:49 +0900

    Add files via upload

--- gan.py
+++ gan.py
@@ -1,13 +1,14 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Reshape
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import UpSampling2D, Convolution2D
+from keras.layers.convolutional import UpSampling2D, Conv2D
 
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import Flatten, Dropout
 
 import math
 import numpy as np
+# import cupy as cp  # cupyは使えたけどkerasでGPU使うときになんか変だぞ
 
 import os
 from keras.datasets import mnist
@@ -15,6 +16,8 @@ from keras.optimizers import Adam
 from PIL import Image
 
 import argparse
+
+# 基本的に画像の配列は[縦, 横, チャンネル数]で書く
 
 
 BATCH_SIZE = 32
@@ -24,31 +27,33 @@ GENERATED_IMAGE_PATH = 'generated_images/'  # 生成画像の保存先
 
 def generator_model():
     model = Sequential()
-    model.add(Dense(input_dim=100, output_dim=1024))
+    model.add(Dense(input_dim=100, units=1024))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Dense(128*7*7))
+    model.add(Dense(128*7*7))  # 100次元の画像を縦7横7チャンネル128の中間表現に変換
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Reshape((128, 7, 7), input_shape=(128*7*7,)))
+    model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))  # 縦7横7チャンネル128の形に変換
+    model.add(UpSampling2D((2, 2)))  # 縦、横を2倍の大きさに変換
+    model.add(Conv2D(64, (5, 5), padding='same'))   # 64個の5*5フィルターで畳み込み、
+                                                    # padding='same'はサイズを変えないようにうまくパディングする　
+                                                    # 縦7横7チャンネル64になった
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(UpSampling2D((2, 2)))
-    model.add(Convolution2D(64, 5, 5, border_mode='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Convolution2D(1, 5, 5, border_mode='same'))
+    model.add(Conv2D(1, (5, 5), padding='same'))
     model.add(Activation('tanh'))
     return model
 
 
 def discriminator_model():
     model = Sequential()
-    model.add(Convolution2D(64, 5, 5,
-                            subsample=(2, 2),
-                            border_mode='same',
-                            input_shape=(1, 28, 28)))
+    model.add(Conv2D(64, (5, 5),
+                     strides=(2, 2),
+                     padding='same',
+                     input_shape=(28, 28, 1)))
     model.add(LeakyReLU(0.2))
-    model.add(Convolution2D(128, 5, 5, subsample=(2, 2)))
+    model.add(Conv2D(128, (5, 5), strides=(2, 2), data_format='channels_first'))
     model.add(LeakyReLU(0.2))
     model.add(Flatten())
     model.add(Dense(256))
@@ -71,7 +76,7 @@ def combine_images(generated_images):
     total = generated_images.shape[0]
     cols = int(math.sqrt(total))
     rows = math.ceil(float(total)/cols)
-    width, height = generated_images.shape[2:]
+    width, height = generated_images.shape[1:3]
     combined_image = np.zeros((height*rows, width*cols),
                               dtype=generated_images.dtype)
 
@@ -82,20 +87,20 @@ def combine_images(generated_images):
     return combined_image
 
 
-
-
 def train():
     (X_train, y_train), (_, _) = mnist.load_data()
     X_train = (X_train.astype(np.float32) - 127.5)/127.5
-    X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1], X_train.shape[2])
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
 
     discriminator = discriminator_model()
+    discriminator.summary()
     d_opt = Adam(lr=1e-5, beta_1=0.1)
     discriminator.compile(loss='binary_crossentropy', optimizer=d_opt)
 
     # generator+discriminator （discriminator部分の重みは固定）
     discriminator.trainable = False
     generator = generator_model()
+    generator.summary()
     dcgan = Sequential([generator, discriminator])
     g_opt = Adam(lr=2e-4, beta_1=0.5)
     dcgan.compile(loss='binary_crossentropy', optimizer=g_opt)
@@ -109,7 +114,7 @@ def train():
             image_batch = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
             generated_images = generator.predict(noise, verbose=0)
 
-            #生成画像を出力
+            # 生成画像を出力
             if index % 500 == 0:
                 image = combine_images(generated_images)
                 image = image*127.5 + 127.5
@@ -118,12 +123,12 @@ def train():
                 Image.fromarray(image.astype(np.uint8))\
                     .save(GENERATED_IMAGE_PATH+"%04d_%04d.png" % (epoch, index))
 
-             #discriminatorを更新
+            # discriminatorを更新
             X = np.concatenate((image_batch, generated_images))
             y = [1]*BATCH_SIZE + [0]*BATCH_SIZE
             d_loss = discriminator.train_on_batch(X, y)
 
-            #generatorを更新
+            # generatorを更新
             noise = np.array([np.random.uniform(-1, 1, 100) for _ in range(BATCH_SIZE)])
             g_loss = dcgan.train_on_batch(noise, [1]*BATCH_SIZE)
             print("epoch: %d, batch: %d, g_loss: %f, d_loss: %f" % (epoch, index, g_loss, d_loss))
@@ -132,7 +137,7 @@ def train():
         discriminator.save_weights('discriminator.h5')
 
 
-def generate(BATCH_SIZE, nice=False):
+def generate(nice=False):
     g = generator_model()
     g.compile(loss='binary_crossentropy', optimizer="SGD")
     g.load_weights('generator')
@@ -140,11 +145,11 @@ def generate(BATCH_SIZE, nice=False):
         d = discriminator_model()
         d.compile(loss='binary_crossentropy', optimizer="SGD")
         d.load_weights('discriminator')
-        noise = np.random.uniform(-1, 1, (BATCH_SIZE*20, 100))
+        noise = np.random.uniform(-1, 1, (BATCH_SIZE * 20, 100))
         generated_images = g.predict(noise, verbose=1)
         d_pret = d.predict(generated_images, verbose=1)
-        index = np.arange(0, BATCH_SIZE*20)
-        index.resize((BATCH_SIZE*20, 1))
+        index = np.arange(0, BATCH_SIZE * 20)
+        index.resize((BATCH_SIZE * 20, 1))
         pre_with_index = list(np.append(d_pret, index, axis=1))
         pre_with_index.sort(key=lambda x: x[0], reverse=True)
         nice_images = np.zeros((BATCH_SIZE,) + generated_images.shape[1:3], dtype=np.float32)
@@ -160,27 +165,23 @@ def generate(BATCH_SIZE, nice=False):
     image = image*127.5+127.5
     Image.fromarray(image.astype(np.uint8)).save("generated_image.png")
 
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", type=str)
-    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--nice", dest="nice", action="store_true")
     parser.set_defaults(nice=False)
     args = parser.parse_args()
     return args
 
+
 if __name__ == "__main__":
     args = get_args()
     if args.mode == "train":
+        print("今trainやで")
         train()
     elif args.mode == "generate":
-        generate(BATCH_SIZE=BATCH_SIZE, nice=args.nice)
-
-
-
-
-
-
-
-
+        print("今generateやで")
+        generate(nice=args.nice)
 
